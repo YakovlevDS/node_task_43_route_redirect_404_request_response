c982a0ddd25c7ec174261fbafd43c6f532e12bfc
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-17 21:29:39 +0300

    feat: add routing reroute response request

--- app.js
+++ app.js
@@ -1,12 +1,69 @@
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let name = process.argv[2];
-let age = process.argv[3];
+// var http = require("http");
+ 
+// http.createServer(function(request, response){
+     
+//     console.log("Url: " + request.url);
+//     console.log("Тип запроса: " + request.method);
+//     console.log("User-Agent: " + request.headers["user-agent"]);
+//     console.log("Все заголовки");
+//     console.log(request.headers);
+//     response.setHeader("UserId", 12);
+//     response.setHeader("Content-Type", "text/html; charset=utf-8;");
+//     response.write("<!DOCTYPE html>");
+//     response.write("<html>");
+//     response.write("<head>");
+//     response.write("<title>Hello Node.js</title>");
+//     response.write("<meta charset=\"utf-8\" />");
+//     response.write("</head>");
+//     response.write("<body><h2>Привет мир</h2></body>");
+//     response.write("</html>");
+//     response.end();
+     
+//     response.end();
+// }).listen(3000);
 
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
-console.log("name: " + name);
-console.log("age: " + age);
+// http://localhost:3000/index.html
 
-// node app.js Tom 23
+// маршрутизация
+
+// const http = require("http");
+  
+// http.createServer(function(request, response){
+     
+//     response.setHeader("Content-Type", "text/html; charset=utf-8;");
+     
+//     if(request.url === "/home" || request.url === "/"){
+//         response.write("<h2>Home</h2>");
+//     }
+//     else if(request.url == "/about"){
+//         response.write("<h2>About</h2>");
+//     }
+//     else if(request.url == "/contact"){
+//         response.write("<h2>Contacts</h2>");
+//     }
+//     else{
+//         response.write("<h2>Not found</h2>");
+//     }
+//     response.end();
+// }).listen(3000);
+// переадресация
+const http = require("http");
+  
+http.createServer(function(request, response){
+     
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+     
+    if(request.url === "/"){
+        response.statusCode = 302; // временная переадресация
+        // на адрес localhost:3000/newpage
+        response.setHeader("Location", "/newpage");
+    }
+    else if(request.url == "/newpage"){
+        response.write("New address");
+    }
+    else{
+        response.statusCode = 404; // адрес не найден
+        response.write("Not Found");
+    }
+    response.end();
+}).listen(3000);
